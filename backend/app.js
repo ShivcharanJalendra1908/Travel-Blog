a168fa0ccc9e33a2867fd32f94acd770c6f55d1a
ShivcharanJalendra1908 <shivcharanjalendra@gmail.com>
2023-08-03 23:32:15 +0530

    Travel Blog

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/user-routes";
+import blogRouter from "./routes/blog-routes";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.set("view engine","ejs");
+
+app.use(express.json());
+app.use("/api/user", userRouter);
+app.use("/api/blog", blogRouter);
+
+app.use("/api", (req,res,next) => {
+  res.send("Hello")
+})
+
+mongoose
+  .connect(
+    "mongodb+srv://Shivcharan:Jalendra@cluster0.bjztio7.mongodb.net/?retryWrites=true&w=majority"
+)
+.then(() => app.listen(5000))
+.then(() => 
+    console.log("Connected To Database and Listening To Localhost")
+)
+.catch((err) => console.log(err));
---
+++ frontend/src/components/Utils.js
@@ -0,0 +1,6 @@
+import { makeStyles } from "@mui/styles";
+export const useStyles = makeStyles({
+    font: {
+        fontFamily: "Dancing Script !important",
+    },
+});
